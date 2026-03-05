ccaebf6a88f4c29ba7b5c56c33a56416df1e7aff
Vinicius Pereira <viniciuspereiraassuncao@gmail.com>
2026-03-05 12:24:33 -0300

    [CREATE] Styling button using "Styled Components" and making a .d.ts to configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
